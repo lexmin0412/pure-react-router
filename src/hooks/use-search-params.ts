6fce48fc00662fc88dac85ba36e95bba010f56ff
lexmin0412 <zhangle_dev@outlook.com>
2024-11-06 15:54:02 +0800

    refactor: remove useless logs

--- src/hooks/use-search-params.ts
+++ src/hooks/use-search-params.ts
@@ -3,7 +3,6 @@ import useHistory from "./use-history";
 const useSearchParams = () => {
   const history = useHistory();
   const searchParams = new URLSearchParams(history.location.search);
-  console.log("searchParams", searchParams);
   return searchParams;
 };
 
--- src/utils/match-route.ts
+++ src/utils/match-route.ts
@@ -2,21 +2,16 @@ import { IRoute } from "../types";
 
 // 定义路由匹配函数
 export const matchRoute = (pathname: string, routes: IRoute[]) => {
-  console.log('enter matchRoute', pathname, routes)
   for (let i = 0; i < routes.length; i++) {
     const route = routes[i];
     const routePath = route.path;
     const routeRegex = routePath
       .replace(/\//g, "\\/")
       .replace(/:\w+/g, "([^/]+)");
-      console.log('routeRegex', routeRegex)
     const regex = new RegExp(`^${routeRegex}`);
 
     const routeParamKeys = routePath.split("/").filter((part) => part.startsWith(":")).map((part) => part.slice(1));
     const routeParamValues = pathname?.match(regex)?.slice(1)
-
-    console.log('key列表', routeParamKeys)
-    console.log('值列表', routeParamValues)
 
     const tempParams: Record<string, string> = {}
     routeParamKeys.forEach((key, index)=>{
