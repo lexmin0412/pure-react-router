daaabdd8c637f28904a045c091ec4eb17bba8e8d
lexmin0412 <zhangle_dev@outlook.com>
2025-03-20 22:58:03 +0800

    refactor: 删除旧的路由相关代码以重构项目结构

--- src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as BrowserRouter } from './browser-router'
-export { default as Link } from './link'
-export { default as Route } from './route'
--- src/hooks/use-search-params.ts
+++
@@ -1,9 +0,0 @@
-import useHistory from "./use-history";
-
-const useSearchParams = () => {
-  const history = useHistory();
-  const searchParams = new URLSearchParams(history.location.search);
-  return searchParams;
-};
-
-export default useSearchParams
